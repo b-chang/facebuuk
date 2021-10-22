44052ed002690aa9f5417fb81e5479a13545f9b0
b-chang <brandochang1993@gmail.com>
2021-10-22 13:33:05 -0700

    fix posts so adding post does not cause full re-render of posts component

--- client/components/Posts.jsx
+++ client/components/Posts.jsx
@@ -8,16 +8,30 @@ const Posts = () => {
   const { allPosts, singlePost } = useSelector((state) => state);
   const dispatch = useDispatch();
   const [displayPosts, setDisplayPosts] = useState(3);
+  const [newlyAddedPosts, setNewlyAddedPosts] = useState([]);
   const displayMorePosts = (n = 5) => {
     setDisplayPosts((prev) => prev + 5);
   };
 
   useEffect(() => {
     dispatch(fetchPosts());
+  }, []);
+
+  useEffect(() => {
+    if (singlePost.loading === 'loaded') {
+      setNewlyAddedPosts((prev) => [...prev, singlePost]);
+    }
   }, [singlePost.loading]);
 
   return (
     <div>
+      <div className="flex flex-col-reverse">
+        {newlyAddedPosts.length
+          ? newlyAddedPosts.map((newPost, idx) => (
+              <Post key={idx} post={newPost.post} />
+            ))
+          : ''}
+      </div>
       {(allPosts.loading === 'loaded' || allPosts.data.length !== 0) && (
         <InfiniteScroll
           className="px-2"
@@ -27,7 +41,11 @@ const Posts = () => {
         >
           <div>
             {allPosts.loading === 'loaded' &&
-              allPosts.data.map((post, idx) => <Post key={idx} post={post} />)}
+              allPosts.data.map((post, idx) => {
+                if (idx < displayPosts) {
+                  return <Post key={idx} post={post} />;
+                }
+              })}
           </div>
         </InfiniteScroll>
       )}
